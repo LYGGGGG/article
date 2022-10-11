f29ebf193b5e5080e60513147b8c3218025706d7
YG <710431075@qq.com>
2022-10-11 16:33:37 +0800

    Initial commit

---
+++ src/main/java/cn/itcast/article/ArticleApplication.java
@@ -0,0 +1,15 @@
+package cn.itcast.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@author YG
+@create 2022/10/11   16:01
+*/
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
